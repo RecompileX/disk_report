1a7f313954a9caeb9780b5f7455e48bc9eff7edc
RecompileX <hamzakamal643@gmail.com>
2026-09-07 00:10:08 -0500

    Added some bugs and made extensions counted

--- main.cpp
+++ main.cpp
@@ -14,8 +14,11 @@ int main(){
     std::string targetDir = "-1";
     std::cin >> targetDir;
 
+    bool fileExists = false;
     std::vector<std::string> directories;
     std::vector<std::string> files;
+    std::vector<std::string> fileExtension;
+    std::vector<int> fileExtensionAmount;
 
     if(fs::exists(targetDir) && targetDir != "-1") {
         for(const auto& dir : fs::recursive_directory_iterator(targetDir)){
@@ -23,16 +26,54 @@ int main(){
             if (!fs::is_directory(dir)) {
                 files.push_back(dir.path().string());
             }
-            else if(fs::is_directory(dir)) {
+                else if(fs::is_directory(dir)) {
+                directories.push_back(dir.path().string());
             }
         }
-        for (int x = 0; x < files.size() - 1; x++){
-            std::cout << files[x] << std::endl;
+        for (int x = 0; x < files.size(); x++){
+            std::string fileExtensionsTMP;
+            fileExtensionsTMP = fs::path(files[x]).extension().string();
+            for (int a = 0; a < fileExtension.size(); a++) {
+                fileExists = false;
+                if (fileExtension[a] == fileExtensionsTMP) {
+                    fileExists = true;
+                    fileExtensionAmount[a]++;
+                    break;
+                }
+            }
+            if (fileExists == false) {
+                fileExtension.push_back(fileExtensionsTMP);
+                fileExtensionAmount.push_back(1);
+            }
+        }
+        // finsih this
+        auto directoriesCopy = directories;
+        for (int x = 0; x < directories.size(); x++) {
+            std::string directoriesTMP;
+            directoriesTMP = directories[x];
+            for (int a = 0; a < directories.size(); a++) {
+                fileExists = false;
+                if (directories[a] == directoriesTMP) {
+                    fileExists = true;
+                    break;
+                }
+            }
 
+            if (fileExists == false) {
+                directories.push_back(directoriesTMP);
+            }
         }
-        for (int x = 0; x < directories.size() - 1; x++) {
-            std::cout << directories[x] << std::endl;
+        std::cout << "Directories:" << std::endl;
+        for (int x = 0; x < directories.size(); x++) {
+            std::cout << (directories[x].substr(0, directories[x].rfind('.'))) << std::endl;
         }
+        std::cout << "End Directories" << std::endl;
+
+        std::cout << "File Extensions:" << std::endl;
+        for (int x = 0; x < fileExtension.size(); x++) {
+            std::cout << fileExtension[x] << ' ' <<fileExtensionAmount[x] << std::endl;
+        }
+        std::cout << "End File Extensions" << std::endl;
     }
     else{
         std::cout << "Invalid Directory!" << std::endl;
